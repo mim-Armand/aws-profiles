2316c9ec154dd687bb659f61605a13dfa11bfacd
mim Armand <mim3dot@gmail.com>
2018-02-27 22:42:07 -0600

    added the use method

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 /* jshint node: true */
 /*jshint esversion: 6 */
+const AWS = require('aws-sdk');
 const SharedIniFile = require('aws-sdk/lib/shared_ini');
 exports.get = function get(){
 	var results  = [];
 	try{ results = new SharedIniFile().getProfiles(); }
 	finally{ return results; }
+}
+exports.use = function use(profile_name){
+	return new AWS.SharedIniFileCredentials({profile: profile_name});
 }
