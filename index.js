a34dcfe0f6e1c05c1b3c35427e765ff2fcd2c8b5
mim Armand <mim3dot@gmail.com>
2018-02-27 21:49:01 -0600

    shortened the get method name ad also fixed a problem in the readme doc

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /* jshint node: true */
 /*jshint esversion: 6 */
 const SharedIniFile = require('aws-sdk/lib/shared_ini');
-exports.getAWSProfiles = function getAWSProfiles(){
+exports.get = function get(){
     return new SharedIniFile().getProfiles();
 
 }
